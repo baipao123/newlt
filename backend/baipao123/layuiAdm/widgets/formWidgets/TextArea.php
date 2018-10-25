b57d97e2255d8337de10216582e00fe5a3a898c0
huangchen <772965632@qq.com>
2018-10-25 10:55:31 +0800

    .

--- backend/baipao123/layuiAdm/widgets/formWidgets/TextArea.php
+++ backend/baipao123/layuiAdm/widgets/formWidgets/TextArea.php
@@ -40,6 +40,7 @@ class TextArea extends InputOptions
             $(function() {
                       window.um = UM.getEditor('{$this->id}', {
                       	 autoFloatEnabled:false,
+                         zIndex:899,
         	/* 传入配置参数,可配参数列表看umeditor.config.js */
             toolbar: ['undo redo | bold italic underline | forecolor backcolor | justifyleft justifyright justifycenter | formula']
         });
